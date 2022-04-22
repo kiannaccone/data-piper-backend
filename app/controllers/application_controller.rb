1a7a0c0dfb7428f37f9349e4193d83fa49ca9781
Katherine Iannaccone <katherineiannaccone@yahoo.com>
2022-04-22 11:50:56 -0600

    completed coding challenge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,28 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/datapipers" do
+    datapipers=Datapiper.all
+    datapipers.to_json
+  end
+
+  get "/datapipers/:id" do
+    datapiper = Datapiper.find(params[:id])
+    datapiper.to_json
+  end 
+
+  post "/datapipers" do
+    datapiper = Datapiper.create(
+      client:params[:client],
+      poc:params[:poc],
+      email:params[:email],
+      role:params[:role],
+      urgency:params[:urgency],
+      quantity:params[:quantity],
+      skills_needed:params[:skills_needed],
+    )
+
+    datapiper.to_json
   end
 
 end
